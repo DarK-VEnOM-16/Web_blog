0cc7c835c50d54d2760b50a11f973e18b17ea7b0
Sahil Bairagi <sahilbairagi319@gmail.com>
2020-05-01 03:03:32 +0530

    Uploaded some more forms

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, length, email, equal_to, ValidationError
 from flaskblog.models import User
-
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -35,3 +36,36 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), email()])
+    picture = FileField('Update profile picture', validators=[FileAllowed(['jpg', 'png', 'jfif'])])
+    submit = SubmitField('Update')
+
+    '''
+    def validate_field(self, field):
+        if True:
+            raise  ValidationError('Validation Message')
+    '''
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('This Username is already taken. Please choose another one.')
+
+    def validate_email(self, email):
+
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('This Email ID is already in use. Please choose another one or try logging in.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
